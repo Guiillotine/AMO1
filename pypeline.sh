64424e5f46b88d3ec45a27c071dbc761d3da716b
Lena <kozlova03@inbox.ru>
2025-03-16 22:58:49 +0700

    python command changed to python3

--- pypeline.sh
+++ pypeline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "-> data_creation.py"
-python data_creation.py
+python3 data_creation.py
 
 echo "-> model_preprocessing.py"
-python model_preprocessing.py
+python3 model_preprocessing.py
 
 echo "-> model_preparation.py"
-python model_preparation.py
+python3 model_preparation.py
 
 echo "-> model_testing.py"
-python model_testing.py
+python3 model_testing.py
